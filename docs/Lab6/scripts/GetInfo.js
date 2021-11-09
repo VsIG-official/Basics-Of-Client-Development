086558836506fc5078fa023ba21c0cf63d107442
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-09 18:59:10 +0200

    ✨: add student info

---
+++ docs/Lab6/scripts/GetInfo.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const SURNAME = "Dominskyi";
+const NAME = "Valentyn";
+
+function showInfo(surname, name, position = "Student") {
+    alert(`Surname - ${surname}, name - ${name} and position - ${position}`);
+}
