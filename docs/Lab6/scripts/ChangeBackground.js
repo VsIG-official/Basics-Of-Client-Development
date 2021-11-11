9efdc7ab26ebeade8e3fcf1ec23468a43f29894d
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-11 17:42:46 +0200

    ♻️: add better theme switching

--- docs/Lab6/scripts/ChangeBackground.js
+++ docs/Lab6/scripts/ChangeBackground.js
@@ -14,12 +14,32 @@ const NEW_BUTTON_TEXT = "Wait 30 seconds";
 const TIME = 30 * 1000;
 
 function changeTheme() {
-    document.body.style.backgroundImage = `url(${NEW_BACKGROUND})`;
+    changeBackground();
 
+    let themeModeText = changeThemeModeText();
+    let themeSwitcher = changeThemeSwitcher();
+
+    changeDefaultThemeAfterTime(themeModeText, themeSwitcher);
+}
+
+function changeBackground() {
+    document.body.style.backgroundImage = `url(${NEW_BACKGROUND})`;
+}
+
+function changeThemeModeText() {
     let themeModeText = document.getElementById(ELEMENT);
     themeModeText.textContent = NEW_ELEMENT_TEXT;
+
+    return themeModeText;
+}
+
+function changeThemeSwitcher() {
     let themeSwitcher = document.getElementById(BUTTON);
     themeSwitcher.textContent = NEW_BUTTON_TEXT;
 
+    return themeSwitcher;
+}
+
+function changeDefaultThemeAfterTime(themeModeText, themeSwitcher) {
     setTimeout(() => (document.body.style.backgroundImage = `url(${DEFAULT_BACKGROUND})`, themeModeText.textContent = DEFAULT_ELEMENT_TEXT, themeSwitcher.textContent = DEFAULT_BUTTON_TEXT), TIME);
 }
