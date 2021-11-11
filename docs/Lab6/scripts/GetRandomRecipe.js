a7f97c46d5b6c3d24c5616e9ecbd28762407e82d
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-11 19:44:26 +0200

    ✨: add querySelectorAll and innerHTML

---
+++ docs/Lab6/scripts/GetRandomRecipe.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var links = {
+    0: "Belgian-Waffles.html",
+    1: "Cracked-Cookies.html",
+    2: "Envelopes-With-Apples.html",
+    3: "Waffle-Tubes.html",
+}
+
+const SELECTOR = "div > p#RandomRecipe";
+
+function insertRandomRecipes() {
+    let paragraphs = getAllParagraphs();
+
+    paragraphs.forEach(paragraph => {
+        changeHTML(paragraph);
+    });
+}
+
+function getAllParagraphs() {
+    let paragraphs = document.querySelectorAll(SELECTOR);
+    return paragraphs;
+}
+
+function getRandomRecipe() {
+    let randomNumber = Math.round(Math.random() * 3);
+    let randomRecipe = links[randomNumber];
+    return randomRecipe;
+}
+
+function changeHTML(paragraph) {
+    let randomRecipe = getRandomRecipe();
+    paragraph.innerHTML = `<a href="${randomRecipe}" target="_blank">Random Recipe</a>`;
+}
