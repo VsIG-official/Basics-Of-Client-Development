73ace8b09128770d774fedd6267a39ec628f2483
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-11 17:38:15 +0200

    ✨: add getelemntbyid and textcontent

--- docs/Lab6/scripts/ChangeBackground.js
+++ docs/Lab6/scripts/ChangeBackground.js
@@ -2,10 +2,24 @@
 
 const DEFAULT_BACKGROUND = "images/Background.jpg";
 const NEW_BACKGROUND = "images/BackgroundLight.jpg";
+
+const ELEMENT = "ThemeMode";
+const DEFAULT_ELEMENT_TEXT = "Now You're in Dark mode";
+const NEW_ELEMENT_TEXT = "Now You're in Light mode";
+
+const BUTTON = "ThemeSwitcher";
+const DEFAULT_BUTTON_TEXT = "Switch to Light mode";
+const NEW_BUTTON_TEXT = "Wait 30 seconds";
+
 const TIME = 30 * 1000;
 
-function changeBackground() {
+function changeTheme() {
     document.body.style.backgroundImage = `url(${NEW_BACKGROUND})`;
 
-    setTimeout(() => document.body.style.backgroundImage = `url(${DEFAULT_BACKGROUND})`, TIME);
+    let themeModeText = document.getElementById(ELEMENT);
+    themeModeText.textContent = NEW_ELEMENT_TEXT;
+    let themeSwitcher = document.getElementById(BUTTON);
+    themeSwitcher.textContent = NEW_BUTTON_TEXT;
+
+    setTimeout(() => (document.body.style.backgroundImage = `url(${DEFAULT_BACKGROUND})`, themeModeText.textContent = DEFAULT_ELEMENT_TEXT, themeSwitcher.textContent = DEFAULT_BUTTON_TEXT), TIME);
 }
