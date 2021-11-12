98f6ec8746d30b43842b3c0f774ef596537d5917
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-12 18:32:31 +0200

    ♻️: add better paragraphs creating

--- docs/Lab6/scripts/AddRecipePlaceholder.js
+++ docs/Lab6/scripts/AddRecipePlaceholder.js
@@ -1,8 +1,27 @@
 'use strict'
 
+const PARAGRAPH_TAG = "p";
+const PARAGRAPH_CLASS = "RandomRecipe";
+
 function addRecipePlaceholder() {
+    let paragraph = getParagraph();
+    let newParagraph = createNewParagraph();
+
+    addParagraph(paragraph, newParagraph);
+}
+
+function getParagraph() {
     let paragraph = document.querySelector(RECIPE_SELECTOR);
-    let newParagraph = document.createElement("P");
-    newParagraph.className = "RandomRecipe";
+    return paragraph;
+}
+
+function createNewParagraph() {
+    let newParagraph = document.createElement(PARAGRAPH_TAG);
+    newParagraph.className = PARAGRAPH_CLASS;
+
+    return newParagraph;
+}
+
+function addParagraph(paragraph, newParagraph) {
     paragraph.after(newParagraph);
 }
